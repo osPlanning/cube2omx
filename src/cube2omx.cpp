8a3ef5482bcfd3489c570177fdc6e015143e50a4
Billy Charlton <bcharlton@psrc.org>
2014-05-09 15:55:06 -0700

    Auto-detect OMX vs. Cube, and require CUBE_MAT_NUMBER attr for converting OMX->Cube
    Sorry this should have been two separate checkins.

--- src/cube2omx.cpp
+++ src/cube2omx.cpp
@@ -15,8 +15,12 @@ int convertH5toMat(char *);
 string get_h5_name(char *filename);
 string get_tpp_name(char *filename);
 
+int generateCubeOrder(map<int,string> &lookup, OMXMatrix* omx, int tables, const char* tnames[]);
+
 int copy_data(TPPMatrix*, OMXMatrix*, int, int, vector<string>&);
-int copy_data(OMXMatrix*, TPPMatrix*, int, int);
+int copy_data(OMXMatrix*, TPPMatrix*, int, int, map<int,string>&);
+
+bool isOMX(char*);
 
 hid_t _memspace = -1;
 hid_t _dataspace = -1;
@@ -25,39 +29,63 @@ int main(int argc, char* argv[])
 {
     // Get cmdline parameters
     // for each input .mat file
-    cout << "\nCube MAT/OMX Converter (built " << __DATE__ << ")\n";
+    cout << "\nCube MAT/OMX Converter (built " << __DATE__ << " " << __TIME__ << ")\n";
     int errors = 0;
 
     if (argc==1) {
 		cout << "\nUsage:  cube2omx.exe  [filename1] [filename2] ...\n";
-		cout << "        - Files ending in .omx or .OMX will be converted from OMX to Cube\n";
-		cout << "        - Files ending in anything else will be converted from Cube to OMX\n\n";
+		cout << "        - Valid OMX files will be converted to Cube format\n";
+		cout << "        - Cube files will be converted to OMX\n\n";
 		exit(0);
     }
 
     for (int i=1; i<argc; i++) {
         char *tpfilename = argv[i];
-        printf("\n\nConverting %s to ",tpfilename);
+        printf("\n\nConverting %s ",tpfilename);
 
         // Figure out which way we're converting:
-        string str(tpfilename);
+        bool is_omx = isOMX(tpfilename);
         int v;
 
-        if (((size_t)str.find(".omx")==string::npos) &&
-            ((size_t)str.find(".OMX")==string::npos)) {
-            v = convertMat2h5(tpfilename);
-        } else {
+        if (is_omx) {
+            printf("to Cube: ");
             v = convertH5toMat(tpfilename);
+        } else {
+            printf("to OMX: ");
+            v = convertMat2h5(tpfilename);
         }
 
-        if (v!=0) {
-            printf(">> Failed to convert %s.",tpfilename);
+        if (v != 0) {
+            printf("\n>> Failed to convert %s.",tpfilename);
             errors += v;
         }
     }
 
     printf("\nDone; %d errors and %d of %d completed.\n",errors,argc-errors-1,argc-1);
 }
+
+
+bool isOMX(char *filename) {
+    htri_t answer = H5Fis_hdf5(filename);
+    if (answer<=0) return false;
+
+    // It's HDF5; is it OMX?
+    hid_t f = H5Fopen(filename,H5F_ACC_RDONLY,H5P_DEFAULT);
+    herr_t exists = H5LTfind_attribute(f, "OMX_VERSION");
+
+    //don't actually care what OMX version it is, yet...
+    //char version[255];
+    //int status = H5LTget_attribute_string(f,"/","OMX_VERSION", version);
+    H5Fclose(f);
+
+    if (exists==0)  {
+        fprintf(stderr,"\n** %s is HDF5, but is not a valid OMX file.\n",filename);
+        exit(2);
+    }
+
+    return true;
+}
+
 
 int convertMat2h5(char *filename) {
     int rows, cols, tables, rtn;
@@ -100,57 +128,99 @@ int convertMat2h5(char *filename) {
     return 0;
 }
 
+int generateCubeOrder(map<int,string> &lookup, OMXMatrix* omx, int tables, const char* tnames[]) {
+    // Make sure there is EXACTLY one table for each CUBE_MAT_NUMBER in the
+    // table range. Fail if there are dupes or missing numbers.
+    boolean quit = false;
+
+    for (int i=0; i<tables;i++) {
+        string tablename(tnames[i]);
+        int cubenum = omx->getCubeNumber(tablename);
+
+        if (cubenum<1) {
+            fprintf(stderr, "\n** Table %s does not have required CUBE_MAT_NUMBER attribute",tnames[i]);
+            quit = true;
+            continue;
+        }
+        if (lookup.count(cubenum)>0) {
+            fprintf(stderr, "\n** Table %s has duplicate CUBE_MAT_NUMBER attribute: %s (%d)",tnames[i],lookup[cubenum].c_str(), cubenum);
+            quit = true;
+            continue;
+        }
+
+        lookup[cubenum] = tablename;
+    }
+
+    if (quit) return -1;
+
+    // And finally make sure we're contiguous
+    bool okay = true;
+    for (int i=1; i<=tables;i++) {
+        if (lookup.count(i)==0) {
+            fprintf(stderr, "\n** CUBE_MAT_NUMBER %d is missing from table range 1-%d",i,tables);
+            okay = false;
+        }
+    }
+    if (okay) return 0;
+    return -1;
+}
+
 int convertH5toMat(char *filename) {
     int zones, tables, rtn;
     TPPMatrix *tpp;
     OMXMatrix *omx;
-    const char* tnames[MAX_TABLES];
+    const char* tnames_native[MAX_TABLES];     // OMX doesn't have any idea about matrix 'order'
+    map<int,string> tnames_cube_lookup; // Cube needs things in a specific order.
+    const char* tnames_cube_order[MAX_TABLES];
 
+    // Open h5 file and get dimensions, table names
+    omx = new OMXMatrix();
+    omx->openFile(filename);
+
+    tables = omx->getTables();
+    zones  = omx->getRows();
+
+    for (int t=1; t<=tables; t++) {
+        tnames_native[t-1]=omx->getTableName(t).c_str();
+    }
+
+    // Verify and set up Cube matrix order from CUBE_MAT_NUMBER attributes
+    int status = generateCubeOrder(tnames_cube_lookup, omx, tables, tnames_native);
+    if (status<0) return 1;
+
+    for (int i=0; i<tables;i++) {
+        tnames_cube_order[i] = tnames_cube_lookup[i+1].c_str();
+    }
+
+    // create TPP file
     try {
-        // Open h5 file and get dimensions, table names
-        omx = new OMXMatrix();
-        omx->openFile(filename);
-
-        tables = omx->getTables();
-        zones  = omx->getRows();
-
-        for (int t=1; t<=tables; t++) {
-            tnames[t-1]=omx->getTableName(t).c_str();
-        }
-
-        // create TPP file
         string tppname = get_tpp_name(filename);
         tpp = new TPPMatrix();
-        tpp->createFile(tables, zones, tnames, tppname.c_str());
-
-        // Copy data
-        rtn = copy_data(omx, tpp, zones, tables);
-
-        /* Close the files. */
-        tpp->closeFile();
-        omx->closeFile();
+        tpp->createFile(tables, zones, tnames_cube_order, tppname.c_str());
 
     } catch (TPPMatrix::FileOpenException&) {
         printf("Can't open %s.",filename);
         return 1;
     }
 
+    // Copy data
+    rtn = copy_data(omx, tpp, zones, tables, tnames_cube_lookup);
+
+    /* Close the files. */
+    tpp->closeFile();
+    omx->closeFile();
+
     return 0;
 }
 
 // Copy from HDF5 to TPP:
-int copy_data(OMXMatrix *omx, TPPMatrix *matrix, int zones, int tables) {
+int copy_data(OMXMatrix *omx, TPPMatrix *matrix, int zones, int tables, map<int,string> &order) {
 
     // Set up some scratch space for reading row data
     double *rowdata = matrix->allocateRowBuffer();
 
     int row;
     printf("\n");
-
-    string tableNames[MAX_TABLES+1];
-    for (int t = 1; t <=tables; t++) {
-        tableNames[t] = omx->getTableName(t);
-    }
 
     // Loop on all rows
     for (row=1;row<=zones;row++) {
@@ -159,7 +229,7 @@ int copy_data(OMXMatrix *omx, TPPMatrix *matrix, int zones, int tables) {
         // Loop for each table
         for (int t=1;t<=tables;t++) {
             // Grab a row of data
-            omx->getRow(tableNames[t], row, rowdata);
+            omx->getRow(order[t], row, rowdata);
 
             // And write it to h5
             matrix->writeRow(t, row, rowdata);
@@ -168,6 +238,7 @@ int copy_data(OMXMatrix *omx, TPPMatrix *matrix, int zones, int tables) {
 
     printf("Zone: %d\n",row-1);
     free(rowdata);
+
     return 0;
 }
 
